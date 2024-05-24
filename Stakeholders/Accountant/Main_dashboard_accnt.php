ddbae23db276be8b907b1ca39b3cbf4ddd078265
rawatchunnilal <cheturawat@gmail.com>
2024-05-24 11:48:51 +0530

    added new form

--- Stakeholders/Accountant/Main_dashboard_accnt.php
+++ Stakeholders/Accountant/Main_dashboard_accnt.php
@@ -12,29 +12,38 @@
         .navbar .col-11 .row .col .border {
             padding: 10px;
         }
-        h6{
+
+        h6 {
             padding: 2px;
             padding-left: 5px;
-            font-size: clamp(12px, 2vw, 16px); /* Responsive font size */
+            font-size: clamp(12px, 2vw, 16px);
+            /* Responsive font size */
             margin-bottom: 0 !important;
         }
+
         p {
-            padding: 2px; /* Adjust padding as needed */
+            padding: 2px;
+            /* Adjust padding as needed */
             padding-left: 5px;
-            font-size: clamp(12px, 2vw, 16px); /* Responsive font size */
+            font-size: clamp(12px, 2vw, 16px);
+            /* Responsive font size */
             margin-bottom: 0 !important;
         }
+
         .heading-with-image {
             display: flex;
             align-items: center;
-            justify-content:space-between;
-        }
-        .heading-with-image img {
-            width: 1em; /* Matches the font size of the heading */
-            height: 1em; /* Matches the font size of the heading */
-            margin-right: 5px; /* Space between image and heading */
+            justify-content: space-between;
         }
 
+        .heading-with-image img {
+            width: 1em;
+            /* Matches the font size of the heading */
+            height: 1em;
+            /* Matches the font size of the heading */
+            margin-right: 5px;
+            /* Space between image and heading */
+        }
     </style>
 </head>
 
@@ -75,128 +84,128 @@
             </ul>
         </div>
         <div id="content">
-        <nav class="navbar navbar-expand-lg navbar-light ">
-        <div class="container-fluid row">
-            <div class="col-1">
-                <button type="button" id="sidebarCollapse" class="navbar-btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-            </div>
-            <div class="col-11">
-                <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-                    <div class="col">
-                        <div class="p-1 border  navigation_options">
-                            <div class="heading-with-image">
-                                <h6>Total Students</h6>
-                            </div>
-                            <p>5643</p>
-                        </div>
+            <nav class="navbar navbar-expand-lg navbar-light ">
+                <div class="container-fluid row">
+                    <div class="col-1">
+                        <button type="button" id="sidebarCollapse" class="navbar-btn">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </button>
                     </div>
-                    <div class="col">
-                        <div class="p-1 border  navigation_options">
-                            <div class="heading-with-image">
-                                <h6>Hostelers</h6>
-                                <img src="../../assets/Group 31.png" alt="icon">
+                    <div class="col-11">
+                        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+                            <div class="col">
+                                <div class="p-1 border  navigation_options">
+                                    <div class="heading-with-image">
+                                        <h6>Total Students</h6>
+                                    </div>
+                                    <p>5643</p>
+                                </div>
                             </div>
-                            <p>5643</p>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="p-1 border navigation_options">
-                            <div class="heading-with-image">
-                                <h6>On Leave</h6>
-                                <img src="../../assets/Group 31.png" alt="icon">
+                            <div class="col">
+                                <div class="p-1 border  navigation_options">
+                                    <div class="heading-with-image">
+                                        <h6>Hostelers</h6>
+                                        <img src="../../assets/Group 31.png" alt="icon">
+                                    </div>
+                                    <p>5643</p>
+                                </div>
                             </div>
-                            <p>4589</p>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="p-1 border  navigation_options">
-                            <div class="heading-with-image">
-                                <h6>Removed</h6>
-                                <img src="../../assets/Group 31.png" alt="icon">
+                            <div class="col">
+                                <div class="p-1 border navigation_options">
+                                    <div class="heading-with-image">
+                                        <h6>On Leave</h6>
+                                        <img src="../../assets/Group 31.png" alt="icon">
+                                    </div>
+                                    <p>4589</p>
+                                </div>
                             </div>
-                            <p>7964</p>
+                            <div class="col">
+                                <div class="p-1 border  navigation_options">
+                                    <div class="heading-with-image">
+                                        <h6>Removed</h6>
+                                        <img src="../../assets/Group 31.png" alt="icon">
+                                    </div>
+                                    <p>7964</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </nav>
+            </nav>
             <div class="below_navbar" id="below_navbar">
-        <div class="row">
-            <div class="col-md-6 col-sm-12 left-col">
-                <div class="container">
-                    <div class="row g-2">
-                        <div class="col-4">
-                            <div class="square-box">
-                                <div class="square-box-content">
-                                    <img src="../../assets/Fees_collection.png" alt="">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 left-col">
+                        <div class="container">
+                            <div class="row g-2">
+                                <div class="col-4">
+                                    <div class="square-box">
+                                        <div class="square-box-content">
+                                            <img src="../../assets/Fees_collection.png" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="square-box">
+                                        <div class="square-box-content">
+                                            <img src="../../assets/New_Admitted_students.png" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="square-box">
+                                        <div class="square-box-content add_student" data-id="add_student" data-target="Custom_Registration.php">
+                                            <img src="../../assets/Add_Student.png" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="square-box">
+                                        <div class="square-box-content">
+                                            <img src="../../assets/Remove_Student.png" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="square-box">
+                                        <div class="square-box-content">
+                                            <img src="../../assets/Complaints.png" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="square-box">
+                                        <div class="square-box-content">
+                                            <img src="../../assets/attendance.png" alt="">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="square-box">
-                                <div class="square-box-content">
-                                    <img src="../../assets/New_Admitted_students.png" alt="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="square-box">
-                                <div class="square-box-content">
-                                    <img src="../../assets/Add_Student.png" alt="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="square-box">
-                                <div class="square-box-content">
-                                    <img src="../../assets/Remove_Student.png" alt="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="square-box">
-                                <div class="square-box-content">
-                                    <img src="../../assets/Complaints.png" alt="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="square-box">
-                                <div class="square-box-content">
-                                    <img src="../../assets/attendance.png" alt="">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row g-2">
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-sm-12 right-col d-flex align-items-center justify-content-center">
-                <div class="container">
-                    <div class="heading-with-image">
-                        <h3>Fees Paid Students</h3>
-                        <img src="../../assets/Group 31.png" alt="icon" class="Fees_paid_students" data-id="Fees_paid_students" data-target="Newly_Admitted_Students.php">
-                    </div>
-                    <div class="search-filter-container">
-                        <input type="text" placeholder="Search...">
-                        <button>Filter</button>
-                    </div>
-                    <div class="container student-list-container">
-                        <div class="student-list" id="student-list">
+                            <div class="row g-2">
 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 right-col d-flex align-items-center justify-content-center">
+                        <div class="container">
+                            <div class="heading-with-image">
+                                <h3>Fees Paid Students</h3>
+                                <img src="../../assets/Group 31.png" alt="icon" class="Fees_paid_students" data-id="Fees_paid_students" data-target="Newly_Admitted_Students.php">
+                            </div>
+                            <div class="search-filter-container">
+                                <input type="text" placeholder="Search...">
+                                <button>Filter</button>
+                            </div>
+                            <div class="container student-list-container">
+                                <div class="student-list" id="student-list">
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
         </div>
     </div>
 
@@ -212,17 +221,17 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
     <script type="text/javascript">
-        $(document).ready(function () {
+        $(document).ready(function() {
             console.log("Document is ready");
 
-            $('#sidebarCollapse').on('click', function () {
+            $('#sidebarCollapse').on('click', function() {
                 console.log("Sidebar collapse button clicked");
                 $('#sidebar').toggleClass('active');
                 $(this).toggleClass('active');
                 console.log("Sidebar and button toggled");
             });
 
-            $('.Fees_paid_students').on('click', function (e) {
+            $('.Fees_paid_students').on('click', function(e) {
                 e.preventDefault();
                 console.log("Fees_paid_students clicked");
                 var targetUrl = $(this).data('target');
@@ -232,21 +241,31 @@
                 loadContent(targetUrl, targetId);
             });
 
-            
+            $('.add_student').on('click', function(e) {
+                e.preventDefault();
+                console.log("add_studentclicked");
+                var targetUrl = $(this).data('target');
+                var targetId = $(this).data('id');
+                console.log("Target URL: " + targetUrl);
+                console.log("Target ID: " + targetId);
+                loadContent(targetUrl, targetId);
+            });
+
+
 
             function loadContent(url, id) {
                 console.log("Loading content from: " + url);
                 $.ajax({
                     url: url,
                     method: 'GET',
-                    success: function (data) {
+                    success: function(data) {
                         console.log("Content loaded successfully");
                         $('#below_navbar').html(data);
                         console.log("Content inserted into #below_navbar");
                         $('#below_navbar').attr('id', id);
                         console.log("Content id changed to: " + id);
                     },
-                    error: function (jqXHR, textStatus, errorThrown) {
+                    error: function(jqXHR, textStatus, errorThrown) {
                         console.log("Error loading content: " + errorThrown);
                         $('#below_navbar').html('<p>An error has occurred: ' + errorThrown + '</p>');
                     }
@@ -255,7 +274,7 @@
         });
     </script>
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
+        document.addEventListener("DOMContentLoaded", function() {
             fetch('fetch_newly_admitted_students.php')
                 .then(response => response.json())
                 .then(data => {
@@ -287,15 +306,15 @@
                     });
 
                     document.querySelectorAll('.approve-btn').forEach(button => {
-                        button.addEventListener('click', function () {
+                        button.addEventListener('click', function() {
                             const studentId = this.getAttribute('data-student-id');
                             fetch('update_student_status.php', {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/x-www-form-urlencoded',
-                                },
-                                body: `student_id=${studentId}`
-                            })
+                                    method: 'POST',
+                                    headers: {
+                                        'Content-Type': 'application/x-www-form-urlencoded',
+                                    },
+                                    body: `student_id=${studentId}`
+                                })
                                 .then(response => response.json())
                                 .then(data => {
                                     if (data.success) {
