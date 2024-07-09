bac13cedc1a69b26a1c0af2b8c28854d8787787d
Atharvawarade <atharvarwarade@gmail.com>
2024-07-09 16:26:28 +0530

    Dashboard updated

---
+++ Stakeholders/Warden/Room_Status.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/Room_Status.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"
+  ></script>
+  <!-- Popper.JS -->
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+    integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+    crossorigin="anonymous"
+  ></script>
+  <!-- Bootstrap JS -->
+  <script
+    src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+    integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+    crossorigin="anonymous"
+  ></script>
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"
+  ></script>
+  </head>
+  <body>
+    <div class="Student_Dashboard">
+      <div class="centraldiv">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="nav_button_item">
+                    <div class="button_type">
+                      Total Students <br />
+                      1233
+                    </div>
+                    <img src="../../assets/Group 31.png" alt="go" />
+                  </div>
+                  <div class="nav_button_item">
+                    <div class="button_type">
+                      Hostelers<br />
+                      1233
+                    </div>
+                    <img src="../../assets/Group 31.png" alt="go" />
+                  </div>
+                  <div class="nav_button_item">
+                    <div class="button_type">
+                      On Leave<br />
+                      1233
+                    </div>
+                    <img src="../../assets/Group 31.png" alt="go" />
+                  </div>
+                  <div class="nav_button_item">
+                    <div class="button_type">
+                      Removed<br />
+                      1233
+                    </div>
+                    <img src="../../assets/Group 31.png" alt="go" />
+                  </div>
+                </div>
+              </div>
+              
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".nav_buttons_container" aria-controls="nav_buttons_container" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              </div>
+            </div>
+          </nav>
+          
+      </div>
+    </div>
+  </body>
+</html>
--- Stakeholders/Warden/checkRooms.php
+++ Stakeholders/Warden/checkRooms.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style></style>
+</head>
+<body>
 <?php
 include '../../php/connection/connect.php';
 
@@ -156,3 +165,5 @@ include '../../php/connection/break.php';
   });
 </script>
  -->
+</body>
+</html>
